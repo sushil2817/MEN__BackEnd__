0840f4a0c87d868bbb041f89a4fe7ab1c0512044
sushil2817 <tony281723@gmail.com>
2024-04-17 23:07:03 +0530

    Learning backend in MERN

---
+++ Full-Stack/frontend/src/App.jsx
@@ -0,0 +1,29 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+
+function App() {
+  const [jokes,setJokes] = useState([])
+  useEffect(()=>{
+    axios.get('/api/jokes').then((res)=>{
+         setJokes(res.data)
+    }).catch((err)=>{
+      console.log(err);
+    })
+  })
+  return (
+    <>
+     <h1>Full Stack APP</h1>
+     <p>JOKES:{jokes.length}</p>
+     {jokes.map((joke,index)=>(
+             <div key={joke.id}>
+                     <h3>{joke.title}</h3>
+                     <p>{joke.content}</p>
+             </div>
+     ))}
+    </>
+  )
+}
+
+export default App
