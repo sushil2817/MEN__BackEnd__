441e50528ee7ce4dd8f194b7b5ea6957ef20cc9c
Sushil Kumawat <tony281723@gmail.com>
2024-08-14 23:22:01 +0530

    connect to backend

--- backend_01/src/index.js
+++ backend_01/src/index.js
@@ -1,5 +1,19 @@
-import mongoose from "mongoose";
-import { DB_NAME } from "./constant";
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+
+
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+connectDB( )
+
+
 
 
 
