1ba82e319d42d056aea29eac731ab3eb6f77a06a
Sushil Kumawat <tony281723@gmail.com>
2024-08-22 21:19:24 +0530

    custom api reference

--- backend_01/src/index.js
+++ backend_01/src/index.js
@@ -4,7 +4,8 @@
 
 // -------------second method---------------
 // require('dotenv').config({path: './env'})
-import dotenv from "dotenv"     
+import dotenv from "dotenv"
+import { app } from "./app.js"; 
 import connectDB from "./db/index.js";   
 
 dotenv.config({
@@ -12,6 +13,18 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+
+  app.listen(process.env.PORT|| 8000,()=>{
+    console.log(`Server is running at port : ${process.env.PORT}`);
+    
+  })
+  
+})
+.catch((err)=>{
+  console.log("Mongodb connection failed!!! ",err);
+  
+})
 
 
 
