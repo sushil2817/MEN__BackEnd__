98d38f50afcf7cf878bd3b8cf50d82557d91219c
Sushil Kumawat <tony281723@gmail.com>
2024-08-26 12:52:23 +0530

    api refrence and middleware

--- backend_01/src/index.js
+++ backend_01/src/index.js
@@ -14,19 +14,15 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-
-  app.listen(process.env.PORT|| 8000,()=>{
-    console.log(`Server is running at port : ${process.env.PORT}`);
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running at port: ${process.env.PORT}`);
     
   })
-  
 })
 .catch((err)=>{
-  console.log("Mongodb connection failed!!! ",err);
+  console.log("Mongo db connection failed!!! ",err);
   
 })
-
-
 
 
 
