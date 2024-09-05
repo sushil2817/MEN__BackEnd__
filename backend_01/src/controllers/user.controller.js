d559e6d591b662aed90d42ca8e9a31bc83a5e620
Sushil Kumawat <tony281723@gmail.com>
2024-09-05 23:10:15 +0530

    fix all register controllerbugs

--- backend_01/src/controllers/user.controller.js
+++ backend_01/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
-import {uplodOnCloudinary} from "../utils/cloudinary.js"
+import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 
@@ -17,10 +17,10 @@ const registerUser = asyncHandler( async (req,res)=>{
     // return response
     
     const {fullName, email, username, password} = req.body
-    console.log("email: ",email);
-    console.log("fullname: ",fullName);
-    console.log("username : ",username);
-    console.log("password : ",password);
+    // console.log("email: ",email);
+    // console.log("fullname: ",fullName);
+    // console.log("username : ",username);
+    // console.log("password : ",password);
 
     // if(fullName === ""){
     //     throw new ApiError(400,"fulname is required")
@@ -34,23 +34,31 @@ const registerUser = asyncHandler( async (req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
     
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [ { username } , { email } ]
     })
 
     if(existedUser){
         throw new ApiError(409, "User with email or username already exits")
     }
+    // console.log(req.files);
+    
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.legth > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400,"Avatar file is required")
     }
 
-    const avatar = await uplodOnCloudinary(avatarLocalPath)
-    const coverImage = await uplodOnCloudinary(coverImageLocalPath)
+    const avatar = await uploadOnCloudinary(avatarLocalPath)
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
     if(!avatar){
         throw new ApiError(400,"Avatar file is required")
