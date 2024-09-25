8990eab0d0cb987d278db5d0a2d68a7f82b40b88
Sushil Kumawat <tony281723@gmail.com>
2024-09-25 08:57:12 +0530

    next step create logout user

--- backend_01/src/controllers/user.controller.js
+++ backend_01/src/controllers/user.controller.js
@@ -146,11 +146,43 @@ const loginUser = asyncHandler(async(req,res)=>{
         .cookie("accessToken",accessToken,options)
         .cookie("refreshToken", refreshToken,options)
         .json(
-            new ApiResponse(200,{user,"loggedInUSer,accessToken,refreshToken"})
+            new ApiResponse(
+                200,
+                {
+                    user:loggedInUSer,accessToken,
+                    refreshToken
+                },
+                "User logged in successfully"
+            )
         )
+})
+
+const logoutUser = asyncHandler(async(req,res)=>{
+    await  User.findByIdAndUpdate(req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(new ApiResponse(200,{},"User logged out successfully"))
 })
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 }
