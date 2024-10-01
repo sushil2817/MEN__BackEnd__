bed7fcd380b38e57f5a51a76b857a0a72daeac7f
Sushil Kumawat <tony281723@gmail.com>
2024-10-01 09:02:39 +0530

    Add user account management endpoints

--- backend_01/src/controllers/user.controller.js
+++ backend_01/src/controllers/user.controller.js
@@ -287,7 +287,7 @@ const updateUserAvatar = asyncHandler(async (req,res)=>{
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
 
-    if(avatar.url){
+    if(!avatar.url){
         throw new ApiError(400, "Error while uploading on avatar")
     }
 
