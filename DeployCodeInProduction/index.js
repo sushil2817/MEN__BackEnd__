9380614c7c4fa6bdfb922b3825554cbced556365
sushil2817 <tony281723@gmail.com>
2025-01-14 22:45:27 +0530

    refactor: improve code formatting in index.js

--- DeployCodeInProduction/index.js
+++ DeployCodeInProduction/index.js
@@ -1,19 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
-app.get('/twitter',(req,res)=>{
-    res.send('sushildotcom')
+app.get('/twitter', (req, res) => {
+  res.send('sushildotcom')
 })
-
-app.get('/login',(req,res)=>{
-    res.send('<h1>Please login at sushildotcom</h1>')
+app.get('/login', (req, res) => {
+  res.send('<h1>Please login at sushildotcom</h1>')
 })
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
